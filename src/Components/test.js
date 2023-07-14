474d360c5ecc5ba30a1ea4291233b7e8c7c27a07
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2023-07-14 14:44:21 +0530

    Update test.js

--- src/Components/test.js
+++ src/Components/test.js
@@ -1,15 +1,15 @@
-// let number = 10;
-// for(let i= 1; i <= number; i++) {
-//     if(i % 3 == 0) {
-//         console.log("fizz")
-//     } else if(i % 5 == 0) {
-//         console.log("buzz")
-//     } else if(i % 3 == 0 && i % 5 == 0) {
-//         console.log("fizzbuzz")
-//     } else {
-//         console.log(i)
-//     }
-// }
+let number = 10;
+for(let i= 1; i <= number; i++) {
+    if(i % 3 == 0) {
+        console.log("fizz")
+    } else if(i % 5 == 0) {
+        console.log("buzz")
+    } else if(i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz")
+    } else {
+        console.log(i)
+    }
+}
 let str = '';
 
   for (let i = 0; i < str.length; i++) {
